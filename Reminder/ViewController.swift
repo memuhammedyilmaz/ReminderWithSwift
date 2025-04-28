2f8870bc13ceeec7a8c6c49957affb770d6f9797
Muhammed Yılmaz <yilmaz34muhammed@icloud.com>
2025-04-28 10:27:19 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Muhammed Yılmaz on 28.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
